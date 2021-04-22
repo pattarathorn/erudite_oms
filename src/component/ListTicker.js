8e3ce22335934293f863ecb0f1b7b0b3b2b13a4a
pattarathorn <pattarathorn.s@ku.th>
2021-04-22 18:32:19 +0700

    update listtickker

---
+++ src/component/ListTicker.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function ListTicker(props){
+    const item = props.props.map((item) => 
+        <div key={item.ticker}>
+            <div>{item.ticker}</div>
+            <div>{item.volumn}</div>
+            <div>{item.start_time}</div>
+            <div>{item.end_time}</div>
+            <div>{item.type}</div>
+
+        </div>
+
+    );
+        
+
+    return (        
+        <div className="font-listticker">
+            {item}
+                    
+        </div>
+            
+    )
+}
+export default ListTicker;
