7b06014344d661957fe73323ed8a0f741e623455
pattarathorn <pattarathorn.s@ku.th>
2021-04-23 18:23:48 +0700

    update css , input form

--- src/component/AddList.js
+++ src/component/AddList.js
@@ -5,7 +5,7 @@ import './style.css';
 
 
 const AddList = ({ initList,  setlist}) => {
-
+    /* --- Type --- */
     const [check,setStatus]=useState(false)
     const [type,setType] = useState('Buy')
     const handleType = () =>{
@@ -17,35 +17,40 @@ const AddList = ({ initList,  setlist}) => {
             setType('Sell')
         }
     }
+
+    /* --- set state element --- */
     const [ticker,setTicker] = useState('')
-    const [volumn,setVolumn] = useState()
+    const [volumn,setVolumn] = useState('')
     const [start_time,setTimestart] = useState('')
     const [end_time,setTimestop] = useState('')
-    const [addstatus,setAddstatus] = useState(0)
 
+    /* --- change state element --- */
     function changeTicker(e){
-        setAddstatus(prevstatus => prevstatus+1)
-        console.log(addstatus)
-        setTicker(e.target.value)
+        setTicker(prevticker => e.target.value)
         console.log(ticker)
     }
+
     function changeVolumn(e){
+        console.log(volumn)
         const re = /^[0-9\b]+$/;
         if (e.target.value === '' || re.test(e.target.value)) {
             setVolumn(e.target.value)
         }
-        console.log(volumn)
     }
+
     function changeTimestart(e){
         setTimestart(e.target.value)
         console.log(start_time)
     }
+
     function changeTimestop(e){
         setTimestop(e.target.value)
         console.log(end_time)
     }
+    /* ----------------------------*/
+
+    /* --- Add element to list --- */
     function handleAdd(e) {
-        
         const newList = initList.concat({ ticker, volumn, start_time, end_time ,type});
         setlist(newList)
         console.log(newList)
@@ -54,53 +59,53 @@ const AddList = ({ initList,  setlist}) => {
 
     return(
         <div >
-                <input className="input-ticker" type = "text" value={ticker} placeholder="Ticker" onChange={changeTicker}/>
-                <input className="input-volomn" type = "text"  pattern="[0-9]*" value={volumn} placeholder="volumn" onChange={changeVolumn}/>
-                <input className="input-timestart" type = "text" value={start_time} placeholder="00:00" onChange={changeTimestart}/>
-                <input className="input-timeend" type = "text" value={end_time} placeholder="24:00" onChange={changeTimestop}/>
-                    <Switch
-                        onChange={handleType}
-                        checked={check}
-                        onColor="#E4E5E5"
-                        offColor="#E4E5E5"
-                        onHandleColor="#1E4B72"
-                        offHandleColor="#FFB216"
-                        height={32}
-                        width={60}
-                        className="input-switch"
-                        id="material-switch"
-                        uncheckedHandleIcon={
-                            <div
-                                style={{
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center",
-                                height: "100%",
-                                fontFamily:'Roboto',
-                                fontSize: 14,
-                                color: "white",}}
-                            >
-                                B
-                            </div>
-                        }
-                        checkedHandleIcon={
-                            <div
-                                style={{
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center",
-                                height: "100%",
-                                fontFamily:'Roboto',
-                                fontSize: 14,
-                                color: "white",}}
-                            >
-                                S
-                            </div>
-                        }
-                            uncheckedIcon=''
-                            checkedIcon=''
-                    />
-                <button className="button-add" onClick={handleAdd} > Add </button>
+            <input className="input-ticker" type = "text" value={ticker} placeholder="Ticker" onChange={changeTicker}/>
+            <input className="input-volomn" type = "text"  pattern="[0-9]*" value={volumn} placeholder="volumn" onChange={changeVolumn}/>
+            <input className="input-timestart" type = "text" value={start_time} placeholder="00:00" onChange={changeTimestart}/>
+            <input className="input-timeend" type = "text" value={end_time} placeholder="24:00" onChange={changeTimestop} />
+                <Switch
+                    onChange={handleType}
+                    checked={check}
+                    onColor="#E4E5E5"
+                    offColor="#E4E5E5"
+                    onHandleColor="#1E4B72"
+                    offHandleColor="#FFB216"
+                    height={32}
+                    width={60}
+                    className="input-switch"
+                    id="material-switch"
+                    uncheckedHandleIcon={
+                        <div
+                            style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            height: "100%",
+                            fontFamily:'Roboto',
+                            fontSize: 14,
+                            color: "white",}}
+                        >
+                            B
+                        </div>
+                    }
+                    checkedHandleIcon={
+                        <div
+                            style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            height: "100%",
+                            fontFamily:'Roboto',
+                            fontSize: 14,
+                            color: "white",}}
+                        >
+                            S
+                        </div>
+                    }
+                        uncheckedIcon=''
+                        checkedIcon=''
+                />
+            <button className="button-add"onClick={handleAdd} disabled={ticker.length<1 || volumn.length <1 || start_time.length <1 || end_time.length <1}> Add </button>
         </div>
     )
 }
--- src/component/ListTicker.js
+++ src/component/ListTicker.js
@@ -3,11 +3,11 @@ import React from 'react'
 function ListTicker(props){
     const item = props.props.map((item) => 
         <div key={item.id}>
-            <div>{item.ticker}</div>
-            <div>{item.volumn}</div>
-            <div>{item.start_time}</div>
-            <div>{item.end_time}</div>
-            <div>{item.type}</div>
+            <div className='show-ticker'>{item.ticker}</div>
+            <div className='show-volumn'>{item.volumn}</div>
+            <div className='show-start-time'>{item.start_time}</div>
+            <div className='show-end-time'>{item.end_time}</div>
+            <div className='show-type'>{item.type}</div>
 
         </div>
 
